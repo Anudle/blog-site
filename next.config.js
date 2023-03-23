0465c1b250f88786f2bcb6c5e172616eaaf09f86
Anu Shrestha <anu@Anus-MacBook-Air.local>
2023-03-23 15:28:24 -0700

    about to deploy

--- next.config.js
+++ next.config.js
@@ -1,15 +1,46 @@
-module.exports = {
-  future: {
-      webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-      // Looks like backward compatibility approach.
-  },
-  webpack(config) {
-      config.resolve.fallback = {
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (PHASE_DEVELOPMENT_SERVER === phase) {
+    return {
+      // future: {
+      // 	webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+      // 	// Looks like backward compatibility approach.
+      // },
+      webpack(config) {
+        config.resolve.fallback = {
           ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
           // by next.js will be dropped. Doesn't make much sense, but how it is
           fs: false, // the solution
+        };
+
+        return config;
+      },
+      env: {
+        USER_NAME: "anu",
+        MONGO_PASSWORD: "514HDesccq5uazzw",
+        CLUSTER_NAME: "cluster0",
+      },
+    };
+  }
+  return {
+    // future: {
+    // 	webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+    // 	// Looks like backward compatibility approach.
+    // },
+    webpack(config) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+        fs: false, // the solution
       };
 
       return config;
-  },
+    },
+    env: {
+      USER_NAME: "anu",
+      MONGO_PASSWORD: "514HDesccq5uazzw",
+      CLUSTER_NAME: "cluster0",
+    },
+  };
 };
